792a2544b5aad2018b6fee1d80cb1ef976e0eecf
balamurugan16 <balamurugan.07@outlook.com>
2024-07-18 09:26:38 +0530

    updated a file [skip ci]

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,7 +9,6 @@ module.exports = {
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
